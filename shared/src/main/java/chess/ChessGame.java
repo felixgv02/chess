8d102ab6a75a15ae68226b15bfa9c5da003174f4
Felix Gil <fgilvega@gmail.com>
2026-02-06 16:57:14 -0700

    implement isInCheckmate to ChessGame.java

--- shared/src/main/java/chess/ChessGame.java
+++ shared/src/main/java/chess/ChessGame.java
@@ -154,7 +154,7 @@ public class ChessGame {
      * @return True if the specified team is in checkmate
      */
     public boolean isInCheckmate(TeamColor teamColor) {
-        throw new RuntimeException("Not implemented");
+        return isInCheck(teamColor) && !canTeamMove(teamColor);
     }
 
     /**
