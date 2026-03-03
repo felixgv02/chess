1aeb5364322df2631e1c16658c8a4b8c26b68c25
Felix Gil <fgilvega@gmail.com>
2026-03-02 20:55:37 -0700

    Implement the other half of GameService.java

--- server/src/main/java/service/GameService.java
+++ server/src/main/java/service/GameService.java
@@ -11,19 +11,41 @@ public class GameService {
         this.gameDAO = gameDAO;
         this.authDAO = authDAO;
     }
-
     public record CreateGameRequest(String gameName) {}
     public record CreateGameResult(int gameID) {}
+
     public CreateGameResult createGame(String authToken, CreateGameRequest req) throws DataAccessException {
         if (authDAO.getAuth(authToken) == null) throw new DataAccessException("Error: unauthorized");
         if (req.gameName() == null || req.gameName().isEmpty()) throw new DataAccessException("Error: bad request");
         int gameID = gameDAO.createGame(req.gameName());
         return new CreateGameResult(gameID);
     }
-
     public record ListGamesResult(Collection<GameData> games) {}
+
     public ListGamesResult listGames(String authToken) throws DataAccessException {
         if (authDAO.getAuth(authToken) == null) throw new DataAccessException("Error: unauthorized");
         return new ListGamesResult(gameDAO.listGames());
+    }
+    public record JoinGameRequest(String playerColor, int gameID) {}
+
+    public void joinGame(String authToken, JoinGameRequest req) throws DataAccessException {
+        AuthData auth = authDAO.getAuth(authToken);
+        if (auth == null) throw new DataAccessException("Error: unauthorized");
+        GameData game = gameDAO.getGame(req.gameID());
+        if (game == null) throw new DataAccessException("Error: bad request");
+        if (req.playerColor() == null || (!req.playerColor().equals("WHITE") && !req.playerColor().equals("BLACK"))) {
+            throw new DataAccessException("Error: bad request");
+        }
+        String whiteUser = game.whiteUsername();
+        String blackUser = game.blackUsername();
+        if (req.playerColor().equals("WHITE")) {
+            if (whiteUser != null) throw new DataAccessException("Error: already taken");
+            whiteUser = auth.username();
+        } else if (req.playerColor().equals("BLACK")) {
+            if (blackUser != null) throw new DataAccessException("Error: already taken");
+            blackUser = auth.username();
+        }
+        GameData updatedGame = new GameData(game.gameID(), whiteUser, blackUser, game.gameName(), game.game());
+        gameDAO.updateGame(updatedGame);
     }
 }
