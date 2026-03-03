d3ee3584b1df7af94f243420c9e136afbc71f5cf
Felix Gil <fgilvega@gmail.com>
2026-03-02 20:15:18 -0700

    Implement MemoryAuthDAO.java

---
+++ server/src/main/java/server/dataaccess/MemoryAuthDAO.java
@@ -0,0 +1,27 @@
+package server.dataaccess;
+import model.AuthData;
+import java.util.HashMap;
+
+public class MemoryAuthDAO  implements AuthDAO{
+    final private HashMap<String, AuthData> auths = new HashMap<>();
+
+    @Override
+    public void createAuth(AuthData auth) throws DataAccessException {
+        auths.put(auth.authToken(), auth);
+    }
+
+    @Override
+    public AuthData getAuth(String authToken) throws DataAccessException {
+        return auths.get(authToken);
+    }
+
+    @Override
+    public void deleteAuth(String authToken) throws DataAccessException {
+        auths.remove(authToken);
+    }
+
+    @Override
+    public void clear() throws DataAccessException {
+        auths.clear();
+    }
+}
