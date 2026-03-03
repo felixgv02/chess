cd80cc245286aafa4c18d86886154a4171a314e0
Felix Gil <fgilvega@gmail.com>
2026-03-02 20:03:53 -0700

    Implement AuthDAO.java and UserDAO.java

---
+++ server/src/main/java/server/dataaccess/AuthDAO.java
@@ -0,0 +1,9 @@
+package server.dataaccess;
+import model.AuthData;
+
+public interface AuthDAO {
+    void createAuth(AuthData auth) throws DataAccessException;
+    AuthData getAuth(String authToken) throws DataAccessException;
+    void deleteAuth(String authToken) throws DataAccessException;
+    void clear() throws DataAccessException;
+}
---
+++ server/src/main/java/server/dataaccess/UserDAO.java
@@ -0,0 +1,8 @@
+package server.dataaccess;
+import model.UserData;
+
+public interface UserDAO {
+    void createUser(UserData u) throws DataAccessException;
+    UserData getUser(String username) throws DataAccessException;
+    void clear() throws DataAccessException;
+}
